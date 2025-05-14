7aba2782bc2dcd0f47d964268587d5792cb22445
MelnykAndriy <kukoban@ukr.net>
2025-05-14 23:09:53 +0300

    my vision on welcom

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to this awesome App.
+          Hey, welcome to turbogram.
         </p>
         <a
           className="App-link"
